8ae45e97047805f4a692c5fc80341a04491ae3ac
alejandratub <alitatc@hotmail.com>
2017-11-17 19:03:55 -0600

    token

---
+++ src/app/guards/tokeninterceptor.ts
@@ -0,0 +1,20 @@
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {AuthService} from '../services/auth.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        authorization: this.auth.getToken()
+      }
+    });
+
+    return next.handle(req);
+  }
+
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,21 @@
 import {Injectable} from '@angular/core';
+import {ApiService} from './api.service';
 
 @Injectable()
 export class AuthService {
   token: string;
 
-  constructor() {
+  constructor(private api: ApiService) {
     this.token = null;
   }
-  login() {
+  login(credentials) {
+    this.api.post('login', credentials);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+  getToken() {
+    return this.token;
   }
 }
